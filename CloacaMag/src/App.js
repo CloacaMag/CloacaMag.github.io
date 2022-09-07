1ee962763c033b6d16956a794a8b9e7acd496812
CloacaMag <william.interlock@gmail.com>
2022-09-07 13:05:56 -0500

    link

--- CloacaMag/src/App.js
+++ CloacaMag/src/App.js
@@ -21,12 +21,12 @@ function App() {
         <p>whoa welcome to our sweetttt hallozine website</p>
         <p>submit your delicious whatever/s to CloacaMag@gmail.com
         </p>
+        <a href='https://mail.google.com/mail/u/0/#inbox?compose=jrjtXVZgPPsVgzmChpClSjkPxxQkXMnLWCRWHPkwXPsNQdQGpwjgpGLbFCKPvJrkWmkgMVSB'>
         <img 
-        className="emailimage" 
-        a href="https://mail.google.com/mail/u/0/#inbox?compose=jrjtXVZgPPsVgzmChpClSjkPxxQkXMnLWCRWHPkwXPsNQdQGpwjgpGLbFCKPvJrkWmkgMVSB" 
+        className="emailimage"  
         src={email}>
         </img>
-
+</a>
         <img 
         className="skeletonimage" 
         src={skeleton}
