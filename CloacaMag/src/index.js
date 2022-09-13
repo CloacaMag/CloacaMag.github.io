4dc883022dbe45407be6d92584bfacf36472ee80
CloacaMag <william.interlock@gmail.com>
2022-09-13 13:35:12 -0500

    revert

--- CloacaMag/src/index.js
+++ CloacaMag/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { HashRouter } from 'react-router-dom';
-
 
 var element = React.createElement('h1',{ className: 'greeting'} , 'Hello, world!');
 
@@ -13,12 +10,9 @@ var element = React.createElement('h1',{ className: 'greeting'} , 'Hello, world!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <HashRouter>
+  <React.StrictMode>
     <App />
-    </HashRouter>
-  
-  
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
